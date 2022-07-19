71e31376f5c6d03baab705bcb14c592059e22eca
LaithAlamat <laithalamat4@gmail.com>
2022-07-19 13:02:21 +0300

    country class created

---
+++ Coders-Airlines/Coders-Airlines/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coders_Airlines.Models
+{
+    public class Country
+    {
+    }
+}
