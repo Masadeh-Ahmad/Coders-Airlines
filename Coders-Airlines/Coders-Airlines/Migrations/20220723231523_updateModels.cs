142bbecb2106d8f72a77f7ecf3b9ddbbc19a1485
Ahmad Masadeh <62568250+MasadehAhmad@users.noreply.github.com>
2022-07-24 02:33:32 +0300

    Flight Booking

---
+++ Coders-Airlines/Coders-Airlines/Migrations/20220723231523_updateModels.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Coders_Airlines.Migrations
+{
+    public partial class updateModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumOfBags",
+                table: "Flights");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumOfBags",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumOfBags",
+                table: "Bookings");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumOfBags",
+                table: "Flights",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- Coders-Airlines/Coders-Airlines/Models/Booking.cs
+++ Coders-Airlines/Coders-Airlines/Models/Booking.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace Coders_Airlines.Models
 {
+    public enum Luggage
+    {
+        One=1,
+        Two=2,
+        Three=3
+    }
     public class Booking
     {
         public int ID { get; set; }
         public int FlightID { get; set; }
         public string UserId { get; set; }
         public int NumOfSeats { get; set; }
+        public Luggage NumOfBags { get; set; }
         public double TotalPrice { get; set; }
         public Flight Flight { get; set; }
         public ApplicationUser User { get; set; }
