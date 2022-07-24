a1d287c2fe4e58b5040b735e81bdbdf8c0eca90d
AbdUlrahmanJaran <abdulrahmanjaran@gmail.com>
2022-07-24 23:29:14 +0300

    Admin Rented Lists

---
+++ Coders-Airlines/Coders-Airlines/Pages/Admin/AdminCarRental.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Coders_Airlines.Data;
+using Coders_Airlines.Models;
+
+namespace Coders_Airlines.Pages.Admin
+{
+    public class AdminCarRentalModel : PageModel
+    {
+        private readonly Coders_Airlines.Data.AirlinesDbContext _context;
+
+        public AdminCarRentalModel(Coders_Airlines.Data.AirlinesDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<CarRental> CarRental { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            CarRental = await _context.CarRentals
+                .Include(c => c.Car)
+                .Include(c => c.User).ToListAsync();
+        }
+        public async Task OnGetDate(DateTime from, DateTime to)
+        {
+            CarRental = await _context.CarRentals
+                .Where(c => c.From > from && c.From < to)
+                .Include(c => c.Car)
+                .Include(c => c.User).ToListAsync();
+        }
+        public async Task<IActionResult> OnPostAsync(DateTime dateFrom, DateTime dateTo)
+        {
+            return RedirectToPage("AdminCarRental", "Date" , new { from = dateFrom, to = dateTo });
+        }
+    }
+}
---
+++ Coders-Airlines/Coders-Airlines/Pages/Admin/AdminFlightBookingList.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Coders_Airlines.Data;
+using Coders_Airlines.Models;
+
+namespace Coders_Airlines.Pages.Admin
+{
+    public class AdminFlightBookingListModel : PageModel
+    {
+        private readonly Coders_Airlines.Data.AirlinesDbContext _context;
+
+        public AdminFlightBookingListModel(Coders_Airlines.Data.AirlinesDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Booking> Booking { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Booking = await _context.Bookings
+                .Include(b => b.Flight)
+                .Include(b => b.User).ToListAsync();
+        }
+        public async Task OnGetDate(DateTime from, DateTime to)
+        {
+            Booking = await _context.Bookings
+                .Where(b => b.Flight.DepartureDate > from && b.Flight.DepartureDate < to)
+                .Include(b => b.Flight)
+                .Include(b => b.User).ToListAsync();
+        }
+        public async Task<IActionResult> OnPostAsync(DateTime dateFrom, DateTime dateTo)
+        {
+            return RedirectToPage("AdminFlightBookingList", "Date", new { from = dateFrom, to = dateTo });
+        }
+    }
+}
