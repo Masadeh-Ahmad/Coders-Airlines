76cd41954356a1ae4c13b1d8b31b60ad67a38b5c
masadeh-Ahmad <ahmad.masadeh0_0@hotmail.com>
2025-02-08 17:08:06 +0300

    add pipeline

--- Coders-Airlines/Coders-Airlines/Startup.cs
+++ Coders-Airlines/Coders-Airlines/Startup.cs
@@ -49,6 +49,7 @@ namespace Coders_Airlines
             {
                 // Our DATABASE_URL from js days
                 string connectionString = Configuration.GetConnectionString("DefaultConnection");
+                Console.WriteLine(connectionString);
                 options.UseSqlServer(connectionString);
             });
             services.AddTransient<IEmail, EmailService>();
